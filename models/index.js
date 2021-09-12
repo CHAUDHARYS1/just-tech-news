72e1c548501f99945c582cfbc2d8cd3647d813e6
Shital Chaudhary <shitalchau10@gmail.com>
2021-09-12 18:33:09 -0500

    create the API routes for comments

--- models/index.js
+++ models/index.js
@@ -2,6 +2,7 @@
 const Post = require('./Post');
 const User = require('./User');
 const Vote = require('./Vote');
+const Comment = require('./Comment');
 
 // create associations
 User.hasMany(Post, {
@@ -32,6 +33,14 @@ Vote.belongsTo(Post, {
   foreignKey: 'post_id'
 });
 
+Comment.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id'
+});
+
 User.hasMany(Vote, {
   foreignKey: 'user_id'
 });
@@ -40,4 +49,4 @@ Post.hasMany(Vote, {
   foreignKey: 'post_id'
 });
 
-module.exports = { User, Post, Vote };
+module.exports = { User, Post, Vote, Comment };
