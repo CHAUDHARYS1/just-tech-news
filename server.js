0312584e52efd7a703004b45e5dfc30e3c0c1f58
Shital Chaudhary <shitalchau10@gmail.com>
2021-10-02 18:13:53 -0500

    add helper functions to handlerbars.js

--- server.js
+++ server.js
@@ -2,13 +2,14 @@ const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
+const helpers = require('./utils/helpers');
 
 const app = express();
 const PORT = process.env.PORT || 3002;
 
 // handlebar
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 
 // session
 const session = require('express-session');
